907cadc0aaa42c05dd2ae0b87effa078fa866d5f
Moataz Mohammady <moataz@wuilt.com>
2020-07-15 05:13:08 +0200

    user can ser,reset amount

--- js/options.js
+++ js/options.js
@@ -1,3 +1,22 @@
 $(() => {
-  console.log("options");
+  function syncAmount() {
+    chrome.storage.sync.get("total", (budget) => {
+      if (budget.total) {
+        $("#total").text(budget.total);
+        //todo add to extnsion icon (like notification)
+      }
+    });
+  }
+  // sync total amount every x amount
+  setInterval(() => {
+    syncAmount();
+  }, 500);
+
+  $("#resetTotal").click(() => {
+    var userAction = confirm("Reset?");
+    if (userAction == true) {
+      chrome.storage.sync.set({ total: 0 });
+      $("#total").text(0);
+    }
+  });
 });
